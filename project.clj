3b721371f5f669a5e9cfe8d825e590f1960b1ca0
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2016-06-30 10:29:00 +0900

    format profiles

--- project.clj
+++ project.clj
@@ -6,5 +6,6 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [proto-repl "0.1.2"]
                  [proto-repl-charts "0.2.0"]]
-  :profiles {:dev {:source-paths ["src" "test"]
-                   :dependencies [[org.clojure/tools.namespace "0.2.11"]]}})
+  :profiles
+  {:dev {:source-paths ["src" "test"]
+         :dependencies [[org.clojure/tools.namespace "0.2.11"]]}})
